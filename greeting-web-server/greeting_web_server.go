2a1b3f18d682dac3382539f2d5da2292f9d76394
bpupadhyaya <bpupadhyaya@gmail.com>
2022-11-24 15:07:26 -0800

    Greeting web server.

---
+++ greeting-web-server/greeting_web_server.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"strings"
+)
+
+func greet(respWriter http.ResponseWriter, req *http.Request) {
+	respWriter.ParseForm()
+	fmt.Println(respWriter.Form)
+	fmt.Println("path: ", respWriter.URL.Path)
+	fmt.Println("scheme: ", respWriter.URL.Scheme)
+	fmt.Println("URL long: ", respWriter.Form["url_long"])
+	for key, value := range respWriter.Form {
+		fmt.Printf("key: %s, value: %s", key, value)
+	}
+}
+
+func main() {
+	http.HandleFunc("/", greet)
+	err := http.ListenAndServe(":9090", nil)
+	if err != nil {
+		log.Fatal("Error from ListenAndServe: ", err)
+	}
+}
