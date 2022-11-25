b767c3f1fe63eb0a1b1ef19812c48bef0724b632
bpupadhyaya <bpupadhyaya@gmail.com>
2022-11-24 21:19:07 -0800

    Simple login flow implementation---supply username and password from a browser using html form, receive the same on the server side.

---
+++ sample-web-app/sample_web_server.go
@@ -0,0 +1,44 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"html/template"
+)
+
+func greet(respWriter http.ResponseWriter, req *http.Request) {
+	req.ParseForm()
+	fmt.Println(req.Form)
+	fmt.Println("path: ", req.URL.Path)
+	fmt.Println("scheme: ", req.URL.Scheme)
+	fmt.Println("URL long: ", req.Form["url_long"])
+	fmt.Println("--------")
+	for key, value := range req.Form {
+		fmt.Println("key: ", key)
+		fmt.Println("value: ", value)
+	}
+	fmt.Fprintf(respWriter, "Message from the server")
+}
+
+
+func login(respWriter http.ResponseWriter, req *http.Request) {
+    fmt.Println("method: ", req.Method)
+    if req.Method == "GET" {
+        t, _ := template.ParseFiles("resources/login.gtpl")
+        t.Execute(respWriter, nil)
+    } else {
+        req.ParseForm()
+        fmt.Println("username: ", req.Form["username"])
+        fmt.Println("password: ", req.Form["password"])
+    }
+}
+
+func main() {
+	http.HandleFunc("/", greet)
+	http.HandleFunc("/login", login)
+	err := http.ListenAndServe(":9090", nil)
+	if err != nil {
+		log.Fatal("Error from ListenAndServe: ", err)
+	}
+}
