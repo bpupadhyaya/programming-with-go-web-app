86b42e56420d5b94119b0473430d84c92b0ebb0b
bpupadhyaya <bpupadhyaya@gmail.com>
2022-11-24 18:42:59 -0800

    Unused import.

--- greeting-web-server/greeting_web_server.go
+++ greeting-web-server/greeting_web_server.go
@@ -7,19 +7,22 @@ import (
 )
 
 func greet(respWriter http.ResponseWriter, req *http.Request) {
-	respWriter.ParseForm()
-	fmt.Println(respWriter.Form)
-	fmt.Println("path: ", respWriter.URL.Path)
-	fmt.Println("scheme: ", respWriter.URL.Scheme)
-	fmt.Println("URL long: ", respWriter.Form["url_long"])
-	for key, value := range respWriter.Form {
-		fmt.Printf("key: %s, value: %s", key, value)
+	req.ParseForm()
+	fmt.Println(req.Form)
+	fmt.Println("path: ", req.URL.Path)
+	fmt.Println("scheme: ", req.URL.Scheme)
+	fmt.Println("URL long: ", req.Form["url_long"])
+	fmt.Println("--------")
+	for key, value := range req.Form {
+		fmt.Println("key: ", key)
+		fmt.Println("value: ", value)
 	}
+	fmt.Fprintf(respWriter, "Message from the server")
 }
 
 func main() {
 	http.HandleFunc("/", greet)
-	err := http.ListenAndServe(":8080", nil)
+	err := http.ListenAndServe(":9090", nil)
 	if err != nil {
 		log.Fatal("Error from ListenAndServe: ", err)
 	}
